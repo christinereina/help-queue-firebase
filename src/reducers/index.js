8445fb2156deedad23ad200808764db1be402c4b
blacchoy@gmail.com <blacchoy@gmail.com>
2020-09-28 11:10:53 -0700

    update entry point file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,29 @@
 import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
+import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { createFirestoreInstance } from 'redux-firestore';
+import firebase from "./firebase";
+
+const store = createStore(rootReducer);
+
+const rrfProps = {
+  firebase,
+  config: {
+        userProfile: "users"
+    },
+  dispatch: store.dispatch,
+  createFirestoreInstance
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById('root')
+)
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
