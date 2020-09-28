d448e4203ed7280d3a89c4d64339e41d6c68badb
blacchoy@gmail.com <blacchoy@gmail.com>
2020-09-28 15:26:58 -0700

    add sign up, in and out

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import formVisibleReducer from './form-visible-reducer';
 import ticketListReducer from './ticket-list-reducer';
 import { combineReducers } from 'redux';
 import { firestoreReducer } from 'redux-firestore';
+import 'firebase/auth';
 
 const rootReducer = combineReducers({
   formVisibleOnPage: formVisibleReducer,
