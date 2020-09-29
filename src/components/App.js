703ae03da712ac236735af81b22906311169dc92
blacchoy@gmail.com <blacchoy@gmail.com>
2020-09-29 15:01:11 -0700

    add styled components

--- src/components/App.js
+++ src/components/App.js
@@ -3,10 +3,12 @@ import Header from "./Header";
 import TicketControl from "./TicketControl";
 import Signin from "./Signin";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from '././Navbar/Navbar';
 
 function App(){
   return ( 
     <Router>
+      <Navbar />
       <Header />
       <Switch>
         <Route path="/signin">
